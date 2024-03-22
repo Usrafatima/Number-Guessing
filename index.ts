930d00945ecad9e598d501e0d34093760b1a9e5c
Yusra <yusra7003@gmail.com>
2024-03-23 03:06:27 +0500

    Push my code to github

---
+++ index.ts
@@ -0,0 +1,20 @@
+#!/usr/bin/env node
+import inquirer from "inquirer"
+// 1) Computer will generate a random number
+
+//2) User input for guessing number
+
+
+const randomNumber = Math.floor(Math.random() * 6 + 1);
+const answers = await inquirer.prompt([
+{
+    name : "userGuessedNumber",
+    type: "Number",
+    message: "Please guess a number between 1-6",
+}
+])
+if (answers.userGuessedNumber === randomNumber){
+    console.log("Congragulations! You guessed correct number")
+}else{
+    console.log("You guessed incorrect number")
+} 
